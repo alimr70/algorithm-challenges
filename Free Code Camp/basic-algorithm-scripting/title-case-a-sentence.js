38e7c98a91e40ffcd0b3a104d6b864a9a98e3c80
Ali Maher <ali.maher.mohammed@gmail.com>
2021-09-18 07:01:35 +0200

    title-case-a-sentence

---
+++ Free Code Camp/basic-algorithm-scripting/title-case-a-sentence.js
@@ -0,0 +1,34 @@
+/* -------------------------------------------------------------------------- */
+/*                                 DESCRIPTION                                */
+/* -------------------------------------------------------------------------- */
+
+/**
+ * Title Case a SentencePassed
+ * 
+ * Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+ * 
+ * For the purpose of this exercise, you should also capitalize connecting words like the and of.
+ */
+
+/* -------------------------------------------------------------------------- */
+/*                                    TESTS                                   */
+/* -------------------------------------------------------------------------- */
+
+/**
+ * titleCase("I'm a little tea pot") should return a string.
+ * titleCase("I'm a little tea pot") should return the string I'm A Little Tea Pot.
+ * titleCase("sHoRt AnD sToUt") should return the string Short And Stout.
+ * titleCase("HERE IS MY HANDLE HERE IS MY SPOUT") should return the string Here Is My Handle Here Is My Spout.
+ */
+
+/* -------------------------------------------------------------------------- */
+/*                                  SOLUTION                                  */
+/* -------------------------------------------------------------------------- */
+
+function titleCase(str) {
+  return str.split(" ")
+    .map(el => { return el.substring(0, 1).toUpperCase() + el.substring(1).toLowerCase(); })
+    .join(" ");
+}
+
+console.log(titleCase("I'm a little tea pot"));
