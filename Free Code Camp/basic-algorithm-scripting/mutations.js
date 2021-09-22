dee1a20ac7e6dfddfcad374171cd2eac222c27c4
Ali Maher <ali.maher.mohammed@gmail.com>
2021-09-22 06:08:17 +0200

    mutations

---
+++ Free Code Camp/basic-algorithm-scripting/mutations.js
@@ -0,0 +1,50 @@
+/* -------------------------------------------------------------------------- */
+/*                                 DESCRIPTION                                */
+/* -------------------------------------------------------------------------- */
+
+/**
+ * MutationsPassed
+ * 
+ * Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+ * 
+ * For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
+ * 
+ * The arguments ["hello", "hey"] should return false because the string hello does not contain a y.
+ * 
+ * Lastly, ["Alien", "line"], should return true because all of the letters in line are present in Alien.
+ */
+
+/* -------------------------------------------------------------------------- */
+/*                                    TESTS                                   */
+/* -------------------------------------------------------------------------- */
+
+/**
+ * mutation(["hello", "hey"]) should return false.
+ * mutation(["hello", "Hello"]) should return true.
+ * mutation(["zyxwvutsrqponmlkjihgfedcba", "qrstu"]) should return true.
+ * mutation(["Mary", "Army"]) should return true.
+ * mutation(["Mary", "Aarmy"]) should return true.
+ * mutation(["Alien", "line"]) should return true.
+ * mutation(["floor", "for"]) should return true.
+ * mutation(["hello", "neo"]) should return false.
+ * mutation(["voodoo", "no"]) should return false.
+ * mutation(["ate", "date"]) should return false.
+ * mutation(["Tiger", "Zebra"]) should return false.
+ * mutation(["Noel", "Ole"]) should return true.
+ */
+
+/* -------------------------------------------------------------------------- */
+/*                                  SOLUTION                                  */
+/* -------------------------------------------------------------------------- */
+
+function mutation(arr) {
+  let result = true;
+  let firstWord = arr[0].toLowerCase().split("");
+  let secondWord = arr[1].toLowerCase().split("");
+  secondWord.map((el) => {
+    firstWord.indexOf(el) === -1 ? result = false : "";
+  });
+  return result;
+}
+
+mutation(["hello", "hey"]);
